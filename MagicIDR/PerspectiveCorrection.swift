ad4c45c64513e219b68d82799ca7e914e64e5b75
ohdair <ohdiar@gmail.com>
2024-02-05 20:36:47 +0900

    feat: PerspectiveCorrection 추가
    - 감지된 사각형을 Perspective으로 보정하여 이미지 반환

--- MagicIDR/PerspectiveCorrection.swift
+++ MagicIDR/PerspectiveCorrection.swift
@@ -5,4 +5,44 @@
 //  Created by 박재우 on 2/5/24.
 //
 
-import Foundation
+import CoreImage
+
+class PerspectiveCorrection: RectangleDetectable {
+
+    private var originImage: CIImage
+
+    init(image: CIImage) {
+        self.originImage = image
+    }
+
+    func correct() -> CGImage? {
+        guard let rectangleFeature = detectRectangle(in: originImage) else {
+            return nil
+        }
+
+        let topLeft = CIVector(cgPoint: rectangleFeature.topLeft)
+        let topRight = CIVector(cgPoint: rectangleFeature.topRight)
+        let bottomLeft = CIVector(cgPoint: rectangleFeature.bottomLeft)
+        let bottomRight = CIVector(cgPoint: rectangleFeature.bottomRight)
+
+        guard let perspectiveFilter = CIFilter(name: "CIPerspectiveCorrection") else {
+            return nil
+        }
+
+        perspectiveFilter.setValuesForKeys([
+            kCIInputImageKey: originImage,
+            "inputTopLeft": topLeft,
+            "inputTopRight": topRight,
+            "inputBottomLeft": bottomLeft,
+            "inputBottomRight": bottomRight
+        ])
+
+        guard let outputImage = perspectiveFilter.outputImage else {
+            return nil
+        }
+
+        let ciContext = CIContext(options: nil)
+
+        return ciContext.createCGImage(outputImage, from: outputImage.extent)
+    }
+}
