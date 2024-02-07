784f2ac1afdc1f9ee9eb56a6fe0c225c7935fbc4
ohdair <ohdiar@gmail.com>
2024-02-07 23:42:48 +0900

    feat: RepointViewController UI 추가

--- MagicIDR/RepointViewController.swift
+++ MagicIDR/RepointViewController.swift
@@ -17,6 +17,36 @@ class RepointViewController: UIViewController {
         return imageView
     }()
 
+    private let backButton = {
+        let imageConfig = UIImage.SymbolConfiguration(pointSize: 17, weight: .bold)
+        let image = UIImage(systemName: "chevron.backward", withConfiguration: imageConfig)
+        var buttonConfig = UIButton.Configuration.plain()
+        buttonConfig.image = image
+        buttonConfig.baseForegroundColor = .main
+        return UIButton(configuration: buttonConfig)
+    }()
+
+    private let completeButton = {
+        let imageConfig = UIImage.SymbolConfiguration(pointSize: 17, weight: .bold)
+        let image = UIImage(systemName: "checkmark", withConfiguration: imageConfig)
+        var buttonConfig = UIButton.Configuration.plain()
+        buttonConfig.image = image
+        buttonConfig.baseForegroundColor = .main
+        return UIButton(configuration: buttonConfig)
+    }()
+
+    private lazy var abilitiesStackView = {
+        let stackView = UIStackView()
+        stackView.addArrangedSubview(backButton)
+        stackView.addArrangedSubview(UIView())
+        stackView.addArrangedSubview(UIView())
+        stackView.addArrangedSubview(UIView())
+        stackView.addArrangedSubview(completeButton)
+        stackView.distribution = .fillEqually
+        stackView.backgroundColor = .black.withAlphaComponent(0.1)
+        return stackView
+    }()
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.setNavigationBarHidden(true, animated: false)
@@ -30,19 +60,26 @@ class RepointViewController: UIViewController {
     }
 
     private func setUI() {
-        view.backgroundColor = .systemPink
-        imageView.image = UIImage(ciImage: ciImage)
+        view.backgroundColor = .white
+        imageView.image = UIImage(ciImage: ciImage, scale: 1, orientation: .right)
         view.addSubview(imageView)
+        view.addSubview(abilitiesStackView)
     }
 
     private func setLayout() {
         imageView.translatesAutoresizingMaskIntoConstraints = false
+        abilitiesStackView.translatesAutoresizingMaskIntoConstraints = false
 
         NSLayoutConstraint.activate([
             imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
             imageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-            imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor, multiplier: 4/3)
+            imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor, multiplier: 4/3),
+
+            abilitiesStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            abilitiesStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            abilitiesStackView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            abilitiesStackView.heightAnchor.constraint(equalToConstant: 70)
         ])
     }
 }
